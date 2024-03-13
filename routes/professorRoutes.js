6f46552725649d766524e990df40ca496a924e75
kheireddine <kheireddine.terfa@gmail.com>
2024-03-13 10:47:54 +0100

    add files structure in public folder

--- routes/professorRoutes.js
+++ routes/professorRoutes.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 const professorController = require('../controllers/professorController')
+//-------------authentication:
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)
 router.use(authController.protect)
+//--------------------theme:
 router
   .route('/theme')
   .post(professorController.addTheme)
-  .get(professorController.getProsessorThemes) //.....
+  .get(professorController.getProsessorThemes)
 router
   .route('/theme/:id')
   .get(professorController.getTheme)
