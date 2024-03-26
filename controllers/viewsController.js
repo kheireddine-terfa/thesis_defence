7fb2e356f5928ad6dc532ebaae589d70bf3d3fbe
kheireddine <kheireddine.terfa@gmail.com>
2024-03-26 12:31:41 +0100

    server side rendering for (supervised binomes) , (theses) . add thesis , delete thesis , profile.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,9 @@
 const Announce = require('../models/announceModel')
 const Premise = require('../models/premiseModel')
+const axios = require('axios')
+const Speciality = require('../models/specialityModel')
+const Field = require('../models/fieldModel')
+
 //--------------------- controllers : ------------------
 exports.getAllAnnounces = async (req, res) => {
   const announces = await Announce.find()
@@ -18,5 +22,35 @@ exports.getPremise = async (req, res) => {
   res.status(200).json({
     status: 'success',
     premise,
+  })
+}
+//--------------:
+exports.getIndexPageProfessor = (req, res) => {
+  res.status(200).render('index', {
+    layout: 'professorLayout',
+  })
+}
+//--------------:
+exports.getProfessorLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    layout: false,
+  })
+}
+//--------------:
+exports.getAddThesisForm = async (req, res) => {
+  const specialities = await Speciality.find()
+  const fields = await Field.find()
+  res.status(200).render('ajoutertheme', {
+    layout: 'professorLayout',
+    specialities,
+    fields,
+  })
+}
+//----------------:
+exports.getProfessorInfos = async (req, res) => {
+  const user = req.user
+  res.status(200).render('users-profile', {
+    layout: 'professorLayout',
+    user,
   })
 }
