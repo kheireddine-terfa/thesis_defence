71a6a6bf467437ee83dce455255d11e37522d4bc
juba02 <youvasin02@gmail.com>
2024-04-17 13:56:09 +0100

    css pour les interfaces des logins

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const viewRoutes = require('./routes/viewRoutes')
 app.use(express.urlencoded({ extended: true }))
 //---- body parser : reading data from body into req.body
 app.use(express.json({ limit: '10kb' })) // the result of post request is undefined without this statment -- i mean middelware
-app.use(cookieParser())
+app.use(cookieParser())    
 // STATIC FILES :
 app.use(express.static(path.join(__dirname, 'public')))
 
-// TEMPLATING ENGINE :
+// TEMPLATING ENGINE :  
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
