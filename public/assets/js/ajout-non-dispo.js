ff8211ad9a2d442a51b2aea4b2815bc31d52aaba
juba02 <youvasin02@gmail.com>
2024-05-07 23:02:28 +0100

    FullCalendar test

---
+++ public/assets/js/ajout-non-dispo.js
@@ -0,0 +1,15 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+  
+    var calendar = new Calendar(calendarEl, {
+      plugins: [ dayGridPlugin ], // Utilisez le plugin de grille journalière
+      // Configuration supplémentaire du calendrier si nécessaire
+    });
+  
+    calendar.render();
+});
+
+  
